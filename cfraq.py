39cd6bd74c09e6a8beed1db1d0ae91d7ff583277
Pablo Ventura <pablogventura@gmail.com>
2021-11-03 16:36:03 -0300

    tiene algun error

--- cfraq.py
+++ cfraq.py
@@ -1,6 +1,21 @@
 from fractions import Fraction as F
 
+#https://en.wikipedia.org/wiki/Continued_fraction
+
 def calculate(values):
-    result = 1
-    for a in values:
-        result = F(result/a)
+    result = F(1/values.pop(-1))
+    while len(values) > 1:
+        a = values.pop(-1)
+        result = F(1,result + a)
+        for a in values:
+            result = F(result/a)
+    result = values[0] + result
+    return float(result)
+    
+
+golden=calculate([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+ 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+ 
+sqrt2 = calculate([1]+[2]*10)
